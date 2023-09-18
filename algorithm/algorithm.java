76ae108902647866db2d5d62e2893b7ce37a45e9
Elliot Omark <elliot.omark@mrjaffesclass.com>
2023-09-18 11:44:35 -0700

    cuuuuum

---
+++ algorithm/algorithm.java
@@ -0,0 +1,24 @@
+
+/**
+ * Write a description of class algorithm here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class algorithm
+{
+    public static void main(String[] args){
+        String[] Walk={"turn left","walk straight until quad","turn left",
+        "walk straight until 100 building end","turn left","walk until 123",
+        "enter"};
+        
+        System.out.println(Walk[1]);
+        System.out.println(Walk[2]);
+        System.out.println(Walk[3]);
+        System.out.println(Walk[4]);
+        System.out.println(Walk[5]);
+        System.out.println(Walk[6]);
+        System.out.println(Walk[7]);
+
+    }
+}
