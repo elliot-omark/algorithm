e2c08f2c1349b0d81d8ae7d9c3ef751c342fbf60
Elliot Omark <elliot.omark@mrjaffesclass.com>
2023-09-26 11:22:28 -0700

    mnmmm

---
+++ algorithm-project/algorithm.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class algorithm here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class algorithm
+{
+    public static void main(String[]args){
+        int i=1;
+        while(i<=100){
+            if(i%5==0&&i%3==0){   
+              
+              System.out.println("FizzBuzz"); 
+              i++;  
+            }
+            if(i%3==0){
+                System.out.println("Fizz");
+                i++;
+            }
+            if(i%5==0){                
+                System.out.println("Buzz");
+                i++;
+            }
+            else{              
+                System.out.println(i);
+                i++;
+            }
+        }
+    }
+
+}
