7e9421c5c9c1ad95ef2ab5f7117f2cc37971fd76
Elliot Omark <elliot.omark@mrjaffesclass.com>
2023-09-29 11:06:47 -0700

    jjjjjj

--- algorithm-project/algorithm.java
+++ algorithm-project/algorithm.java
@@ -1,10 +1,5 @@
 
-/**
- * Write a description of class algorithm here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
+
 public class algorithm
 {
     public static void main(String[]args){
